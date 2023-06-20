4ba27824d423ab6e400375989ed0e36f41e733b7
9133sranjitha <sranjitha053@gmail.com>
2023-06-20 23:39:18 +0530

    feat:mastery

--- client side(Front_End)/solidarity/src/App.js
+++ client side(Front_End)/solidarity/src/App.js
@@ -1,21 +1,24 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
-import LadingPage from './Components/landing';
+// import LadingPage from './Components/landing';
 // import LoginPage from './Components/login';
 // import SignUpPage from './signup';
 // import ForGotPage from './forgot';
 // import HomePage from './home';
+import FinancialMastery from './FinancialMastery';
 
 function App() {
   return (
-    <div className="App">
-      <LadingPage />
+    <Router>
+      {/* <LadingPage /> */}
       {/* <LoginPage /> */}
       {/* <SignUpPage /> */}
       {/* <ForGotPage /> */}
       {/* <HomePage /> */}
+      <FinancialMastery />
 
-    </div>
+    </Router>
   );
 }
 
